1d5b75cd29902a04242bc4e463b63c51f5e8c2cb
adushinovaekaterina <adushinova2002@mail.ru>
2023-08-12 20:03:39 +0800

    Создала подключение к пустой БД

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudentRating
 {
     public partial class Form1 : Form
     {
+        // поле для создания подключения к БД
+        // благодаря классу SqlConnection происходят все операции с БД
+        // через созданное открытое подключение
+        private SqlConnection sqlConnection = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // для того чтобы вытащить строку подключения из файла настроек нужно прописать это:
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringDatabaseStudentRating"].ConnectionString);
+            // нужен класс ConfigurationManager и его свойство ConnectionStrings
+            // (это словарь и по ключу мы можем получать значения
+            // ключом является "ConnectionStringsDatabaseStudentRating")
+            // но если не написать в конце ConnectionString, то вернется вся строка подключения
+            // а у нее есть имя, сама строка подключения = путь к БД (она нам и нужна)
+
+            // открываем подключение к БД, иначе ничего не будет работать
+            sqlConnection.Open();
+
+            // проверка, установилось ли подключение к БД
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение к БД установлено!");
+            }
         }
     }
 }
